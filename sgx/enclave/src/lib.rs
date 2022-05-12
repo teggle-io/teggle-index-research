3e098a7adee44d79cbc5a4a91f26a3ab4a39dfd6
David Radunz <david@vimtura.com>
2022-05-12 00:59:07 -0400

    Added various tests / examples for encryption and key scrambling

--- sgx/enclave/src/lib.rs
+++ sgx/enclave/src/lib.rs
@@ -34,8 +34,17 @@ extern crate enclave_ffi_types;
 extern crate lazy_static;
 extern crate alloc;
 extern crate log;
+extern crate ring;
+extern crate blake2;
+extern crate sha2;
+extern crate digest;
 
 use alloc::string::ToString;
+use blake2::{VarBlake2b};
+use blake2::digest::{Input, VariableOutput};
+use digest::FixedOutput;
+use ring::aead::{Aad, LessSafeKey as Key, Nonce, UnboundKey, CHACHA20_POLY1305};
+use ring::hkdf;
 use sgx_types::*;
 use std::string::String;
 //use std::untrusted::fs::File;
@@ -200,20 +209,153 @@ pub extern "C" fn perform_test() -> sgx_status_t {
         keys.push(val);
     }
 
-    /*
-    for k in keys.iter() {
-        db_put(k, k).expect("failed to set db");
-    }
+    // 450ms (overhead for producing test keys).
 
-    db_flush().expect("failed to flush db");
-     */
+    //// Key Scrambling
 
-    for k in keys.iter() {
-        //let _value = db_get(k).expect("failed to get db");
-        let _value = db_get_fixed(k, 32).expect("failed to get db");
+    // Test with blake2b (1350ms, so 900ms)
+    //scramble_with_blake2b(keys);
 
-        //println!("VALUE: {:?}", value.unwrap());
-    }
+    // Test with sha256 (1133ms, so 683ms)
+    //scramble_with_sha256(keys);
+
+    // Test with hkdf (1425ms, so 975ms)
+    //scramble_with_hkdf(keys);
+
+    // Test with uuid v5 (700ms, so 250ms)
+    //scramble_with_uuid_v5(keys);
+
+    //// Encryption
+    // Test with chacha20poly1305 (3150ms, so 2700ms)
+    encrypt_with_chacha20poly1305(keys);
 
     sgx_status_t::SGX_SUCCESS
+}
+
+//// Key Scrambling
+
+// Key scrambling tests.
+#[allow(dead_code)]
+fn scramble_with_hkdf(keys: Vec<[u8; 32]>) {
+    let salt = hkdf::Salt::new(hkdf::HKDF_SHA256, b"test salt");
+    let pkr = salt.extract(b"test secret");
+
+    for k in keys.iter() {
+        let ScrambledKey(_out) = pkr.expand(&[k], ScrambledKey(k.len()))
+            .unwrap()
+            .into();
+
+        //print_result(k, out.as_slice());
+    }
+}
+
+#[allow(dead_code)]
+fn scramble_with_blake2b(keys: Vec<[u8; 32]>) {
+    let my_key= b"test secret";
+
+    for k in keys.iter() {
+        let mut hash = VarBlake2b::new_keyed(my_key, 32);
+        hash.input(k);
+
+        hash.variable_result(|_res| {
+            // TODO:
+            //print_result(k, res);
+        });
+    }
+}
+
+#[allow(dead_code)]
+fn scramble_with_sha256(keys: Vec<[u8; 32]>) {
+    let my_key= b"test secret";
+
+    for k in keys.iter() {
+        let mut hash = sha2::Sha256::default();
+        hash.input(my_key);
+        hash.input(k);
+
+        let _res = hash.fixed_result();
+
+        //print_result(k, res.as_slice());
+    }
+}
+
+#[allow(dead_code)]
+fn scramble_with_uuid_v5(keys: Vec<[u8; 32]>) {
+    let priv_ns = Uuid::parse_str("6042dc53-9d3d-424f-8437-26c0e5abf043").unwrap();
+
+    for k in keys.iter() {
+        let _new_key = Uuid::new_v5(&priv_ns, k);
+    }
+}
+
+// Length overhead:
+//   AEAD = 16 bytes
+//   NOONCE = 12 bytes
+//
+fn encrypt_with_chacha20poly1305(keys: Vec<[u8; 32]>) {
+    let unbound_key = UnboundKey::new(&CHACHA20_POLY1305,
+                                      b"an example very very secret key.")
+        .expect("failed to make key");
+    let key = Key::new(unbound_key);
+
+    let seed = 1234_u32;
+    let mut count = 0_u64;
+    for k in keys.iter() {
+        let adata = k; // TODO:
+        let mut buffer = Vec::from(k.to_vec());
+        //buffer.extend_from_slice(&k[..]);
+
+        let mut nonce_val = [0u8; 12];
+        nonce_val[0..4].copy_from_slice(&seed.to_be_bytes());
+        nonce_val[4..12].copy_from_slice(&count.to_be_bytes());
+
+        let nonce = Nonce::try_assume_unique_for_key(&nonce_val[..])
+            .expect("failed to make noonce"); // 12-bytes; unique per message
+
+        key.seal_in_place_append_tag(nonce, Aad::from(&adata), &mut buffer)
+            .expect("failed to seal");
+
+        let mut res = Vec::with_capacity(nonce_val.len() + buffer.len());
+        res.extend(&nonce_val[..]);
+        res.extend(buffer);
+
+        //buffer.splice(0..0, nonce_val.into_iter());
+
+        //println!("NOONCE: {}, OLD: {}, NEW: {}", nonce_val.len(), k.len(), buffer.len());
+        //print_result(k, res.as_slice());
+        //print_result(k, &nonce_val[..]);
+
+        count = count + 1;
+    }
+}
+
+#[allow(dead_code)]
+fn print_result(orig: &[u8], sum: &[u8]) {
+    for byte in orig {
+        print!("{:02x}", byte);
+    }
+    print!(" => ");
+    for byte in sum {
+        print!("{:02x}", byte);
+    }
+    println!("");
+}
+
+// HKDF
+
+#[derive(Debug, PartialEq)]
+struct ScrambledKey<T: core::fmt::Debug + PartialEq>(T);
+
+impl hkdf::KeyType for ScrambledKey<usize> {
+    fn len(&self) -> usize {
+        self.0
+    }
+}
+
+impl From<hkdf::Okm<'_, ScrambledKey<usize>>> for ScrambledKey<Vec<u8>> {
+    fn from(okm: hkdf::Okm<ScrambledKey<usize>>) -> Self {
+        let mut r = vec![0u8; okm.len().0];
+        okm.fill(&mut r).unwrap();
+        Self(r)
+    }
 }
